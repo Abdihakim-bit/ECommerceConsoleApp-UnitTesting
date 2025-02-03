af73994a720f38a2d082b752ca356c5af5fab59d
abdyh <aali3585@conestogac.on.ca>
2025-02-03 18:26:01 -0500

    Added the complete Product class

---
+++ ECommerceConsoleApp/product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceConsoleApp
+{
+    public class Product
+    {
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
+        {
+
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+
+        public void IncreaseStock(int amount)
+        {
+            StockAmount += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (StockAmount >= amount)
+                StockAmount -= amount;
+            else
+                throw new ArgumentException("Not enough stock to decrease.");
+        }
+
+    }
+}
